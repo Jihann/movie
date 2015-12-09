f6240604125ac96c65d29a6481772d3d54045e4e
Jihann <1244098192@qq.com>
2015-12-09 22:46:18 +0800

    test jade demo

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+//测试 jade语法
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/test', function(req, res, next) {
+  res.render('jade/index');
 });
 
 module.exports = router;
